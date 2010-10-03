ae781f4f27d39abba063fabf09693c7d88afc491
darrellt <darrellt@2e06ed54-2434-11df-be41-57e7d1eefd51>
2010-10-03 02:21:12 +0000

    Add Apache License block and contribtor list

--- remote_control/CellJoust/src/com/allthingsgeek/celljoust/MainActivity.java
+++ remote_control/CellJoust/src/com/allthingsgeek/celljoust/MainActivity.java
@@ -1,3 +1,35 @@
+/*
+ * Celljoust, a cellbot remote control program
+ * 
+ * Copyright (C) 2010 All Things Geek LLC, portions may be copyright others as noted below
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ * 
+ * Contributors:  darrell@allthingsgeek.com (Darrell Taylor), --author of CellJoust
+ *                eric@allthingsgeek.com (Eric Hokanson) --author of ServoBot
+ *                clchen@google.com (Charles L. Chen)  --author of RemoteEye
+ *                chaitanyag@google.com (Chaitanya Gharpure) --author Celldroid
+ *                
+ *                
+ * Portions of this code are based on RemoteEye 
+ * RemoteEye is Apache Licensed and Copyright 2010 Google Inc.                 
+ * 
+ * Portions of this code are based on Celldroid 
+ * Celldroid is Apache Licensed and Copyright 2010 Google Inc.                 
+ * 
+ */
+
+
 package com.allthingsgeek.celljoust;
 
 import java.io.ByteArrayOutputStream;
